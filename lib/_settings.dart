1cea0964c24a34fdcb2a5d1a3feecf1d9e5a5175
Albert Kaiser <ak@kaiserv.de>
2023-03-02 22:14:20 +0100

    flutter 3.7.6 upgrade (#5)

--- lib/_settings.dart
+++ lib/_settings.dart
@@ -12,5 +12,4 @@ const //
     mainBackgroundColor = Color(0xfff2f2f2),
     mainTODO_0 = Color(0xff626665),
     mainTODO_1 = Color(0xffc4b3a3),
-    mainTODO_2 = Color(0xffece9d8)
-;
+    mainTODO_2 = Color(0xffece9d8);
