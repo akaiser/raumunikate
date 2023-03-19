f3077f2cfdfe4443327c312ad402794075b49f69
Albert Kaiser <ak@kaiserv.de>
2023-03-19 05:47:47 +0100

    make it fail

--- test/pages/_shared/extensions/iterable_ext_test.dart
+++ test/pages/_shared/extensions/iterable_ext_test.dart
@@ -87,7 +87,7 @@ void main() {
     test('removes nulls', () {
       expect(
         const [0, 1, null, null, 4, 5].whereNotNull(),
-        const [0, 1, 4, 5],
+        const [0, 1, 4, 6],
       );
     });
   });
