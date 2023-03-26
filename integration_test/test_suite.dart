51b76c48545d6fbb35aff8d8c2e44a5decb53aab
Albert Kaiser <ak@kaiserv.de>
2023-03-26 01:35:01 +0100

    extend ui tests for home page sections (#20)

--- integration_test/test_suite.dart
+++ integration_test/test_suite.dart
@@ -1,9 +1,11 @@
 import 'package:integration_test/integration_test.dart';
 
-import 'pages/navigation_test.dart' as navigation_test;
+import 'navigation/navigation_test.dart' as navigation_test;
+import 'pages/home_page_test.dart' as home_page_test;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   navigation_test.main();
+  home_page_test.main();
 }
