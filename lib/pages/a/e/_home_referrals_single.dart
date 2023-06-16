2460c85692d8fe862cb66781dece06cda9a93f01
Albert Kaiser <ak@kaiserv.de>
2023-06-17 00:32:53 +0200

    build web in profile mode (#48)

--- lib/pages/a/e/_home_referrals_single.dart
+++ lib/pages/a/e/_home_referrals_single.dart
@@ -3,24 +3,22 @@ import 'package:raumunikate/_settings.dart';
 import 'package:raumunikate/pages/a/e/_data.dart' as data;
 import 'package:raumunikate/pages/a/e/_home_referral_card.dart';
 
-class HomeReferralsSingle extends StatelessWidget {
-  const HomeReferralsSingle();
-
-  @override
-  Widget build(BuildContext context) => const Padding(
-        padding: EdgeInsets.all(navigationBarHeight),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                HomeReferralCard(data.referralData0),
-                HomeReferralCard(data.referralData1),
-              ],
-            ),
-            HomeReferralCard(data.referralData2),
-          ],
-        ),
-      );
+class HomeReferralsSingle extends Padding {
+  const HomeReferralsSingle()
+      : super(
+          padding: const EdgeInsets.all(navigationBarHeight),
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  HomeReferralCard(data.referralData0),
+                  HomeReferralCard(data.referralData1),
+                ],
+              ),
+              HomeReferralCard(data.referralData2),
+            ],
+          ),
+        );
 }
