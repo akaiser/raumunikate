8cdd8f695e8d953dabb603f824782d8cd74e2d24
Albert Kaiser <ak@kaiserv.de>
2023-07-27 00:14:20 +0200

    webp

--- lib/_images.dart
+++ lib/_images.dart
@@ -7,58 +7,57 @@ class Images {
       _iconsRoot = '$_root/icons';
 
   static const // home
-      homeIntro = '$_root/home_intro.jpg',
-      homeWelcomeIna = '$_root/home_welcome_ina.jpg',
-      //homeContent = '$_root/home_content.png',
-      homeImage = '$_root/home_image.jpg';
+      homeIntro = '$_root/home_intro.webp',
+      homeWelcomeIna = '$_root/home_welcome_ina.webp',
+      homeImage = '$_root/home_image.webp';
 
   static const // plan
-      planIntro = '$_root/plan_intro.jpg',
-      planStyle = '$_root/plan_style.png',
-      planAtmosphere = '$_root/plan_atmosphere.png',
-      planNaturalFirst = '$_root/plan_natural_first.png',
-      planNaturalSecond = '$_root/plan_natural_second.png',
-      planElegantFirst = '$_root/plan_elegant_first.png',
-      planElegantSecond = '$_root/plan_elegant_second.png';
+      planIntro = '$_root/plan_intro.webp',
+      planStyle = '$_root/plan_style.webp',
+      planAtmosphere = '$_root/plan_atmosphere.webp',
+      planNaturalFirst = '$_root/plan_natural_first.webp',
+      planNaturalSecond = '$_root/plan_natural_second.webp',
+      planElegantFirst = '$_root/plan_elegant_first.webp',
+      planElegantSecond = '$_root/plan_elegant_second.webp';
 
   static const // projects
-      projectsIntro = '$_root/projects_intro.jpg';
+      projectsIntro = '$_root/projects_intro.webp';
 
   static const // products
-      productsIntro = '$_root/products_intro.jpg';
+      productsIntro = '$_root/products_intro.webp';
 
   static const // portrait
-      portraitIntro = '$_root/portrait_intro.jpg',
-      portraitAbout = '$_root/portrait_about.jpg',
-      portraitPassion = '$_root/portrait_passion.jpg';
+      portraitIntro = '$_root/portrait_intro.webp',
+      portraitAbout = '$_root/portrait_about.webp',
+      portraitPassion = '$_root/portrait_passion.webp';
 
   static const // inspiration
-      inspirationIntro = '$_root/inspiration_intro.jpg',
-      inspirationWelcome = '$_root/inspiration_welcome.jpg',
-      inspirationImagination = '$_root/inspiration_imagination.jpg';
+      inspirationIntro = '$_root/inspiration_intro.webp',
+      inspirationWelcome = '$_root/inspiration_welcome.webp',
+      inspirationImagination = '$_root/inspiration_imagination.webp';
 
   static const // blogs
       _blog0 = '$_blogRoot/0';
 
   static const // blog 0
-      child = '$_blog0/child.jpg',
-      eat = '$_blog0/eat.jpg',
-      lilli = '$_blog0/lilli.jpg',
-      sleep = '$_blog0/sleep.jpg',
-      sleep2 = '$_blog0/sleep2.jpg',
-      kitchen = '$_blog0/kitchen.jpg';
+      child = '$_blog0/child.webp',
+      eat = '$_blog0/eat.webp',
+      lilli = '$_blog0/lilli.webp',
+      sleep = '$_blog0/sleep.webp',
+      sleep2 = '$_blog0/sleep2.webp',
+      kitchen = '$_blog0/kitchen.webp';
 
   static const // icons
-      facebook = '$_iconsRoot/facebook.png',
-      instagram = '$_iconsRoot/instagram.png',
-      mail = '$_iconsRoot/mail.png',
-      menu = '$_iconsRoot/menu.png',
-      arrowUp = '$_iconsRoot/arrow_up.png';
+      facebook = '$_iconsRoot/facebook.webp',
+      instagram = '$_iconsRoot/instagram.webp',
+      mail = '$_iconsRoot/mail.webp',
+      menu = '$_iconsRoot/menu.webp',
+      arrowUp = '$_iconsRoot/arrow_up.webp';
 
   static const // misc
-      logo = '$_root/logo.png',
-      logoText = '$_root/logo_text.png',
-      chevron = '$_root/chevron.png',
-      waben0 = '$_root/waben0.jpg',
-      waben1 = '$_root/waben1.jpg';
+      logo = '$_root/logo.webp',
+      logoText = '$_root/logo_text.webp',
+      chevron = '$_root/chevron.webp',
+      waben0 = '$_root/waben0.webp',
+      waben1 = '$_root/waben1.webp';
 }
