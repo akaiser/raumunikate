4ace6ac920cadd80e92e0f2873e08e9ea24f1cf8
akaiser <akaiser@users.noreply.github.com>
2023-11-25 23:48:50 +0000

    deploy: 243d2bf48116590835625ad1903089994aac9931

--- contact.php
+++ contact.php
@@ -2,27 +2,36 @@
 
 use PHPMailer\PHPMailer\PHPMailer;
 
+//use PHPMailer\PHPMailer\SMTP;
+
 require 'inc/PHPMailer/Exception.php';
 require 'inc/PHPMailer/PHPMailer.php';
 require 'inc/PHPMailer/SMTP.php';
 
 if (array_key_exists('email', $_POST)) {
-    date_default_timezone_set('Etc/UTC');;
+    date_default_timezone_set('Etc/UTC');
+
+    $sender_host = '<sender_host>';
+    $sender_email = '<sender_email>';
+    $sender_pwd = '<sender_pwd>';
+    $target_email = '<target_email>';
 
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
 
     $mail = new PHPMailer();
-    $mail->Host = 'smtp.strato.de';
+    $mail->isSMTP();
+    $mail->Host = $sender_host;
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
     $mail->SMTPSecure = 'ssl';
     $mail->SMTPAuth = true;
     $mail->Port = 465;
-    $mail->Username = '<fix_me>';
-    $mail->Password = '<fix_me>';
+    $mail->Username = $sender_email;
+    $mail->Password = $sender_pwd;
 
-    $mail->setFrom($email, $name);
-    $mail->addAddress('ak@kaiserv.de');
+    $mail->setFrom($sender_email, 'Web-Kontaktanfrage');
+    $mail->addAddress($target_email);
     $mail->addReplyTo($email, $name);
     $mail->isHTML(false);
     $mail->Subject = "Web-Kontaktanfrage: $name";
@@ -31,5 +40,6 @@ if (array_key_exists('email', $_POST)) {
     $mail->Body .= "Nachricht:\n$message";
 
     http_response_code($mail->send() ? 201 : 500);
-    exit();
 }
+
+exit();
