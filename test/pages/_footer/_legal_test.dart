1079470e2326c99e19b365109118787685c1d3fa
Albert Kaiser <ak@kaiserv.de>
2025-01-07 00:13:37 +0100

    maintenance

--- test/pages/_footer/_legal_test.dart
+++ test/pages/_footer/_legal_test.dart
@@ -13,7 +13,7 @@ void main() {
       await tester.render(const CopyrightText());
 
       expect(
-        find.text('© 2024 Raumunikate. All rights reserved.'),
+        find.text('© 2025 Raumunikate. All rights reserved.'),
         findsOneWidget,
       );
     });
